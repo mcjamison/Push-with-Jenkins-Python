b01fa6ede882feb710f17a33da5dc9a8945331f1
Jamie Gray <jamisong@google.com>
2015-03-31 15:03:17 -0700

    Add a goodbye handler

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+class ByePage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Goodbye, Cruel World!')
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/bye', ByePage),
 ], debug=True)
