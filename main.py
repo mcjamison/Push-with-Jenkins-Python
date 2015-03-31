b4542b247e8efd963172d3a6f58c5cb74c383b91
Jamie Gray <jamisong@google.com>
2015-03-31 15:17:02 -0700

    Vague comment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
+# Every hello leads to a goodbye...
 class ByePage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
