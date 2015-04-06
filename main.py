60330bc5b2909644c9f4a74b7c398017c6864ed3
Jamie Gray <jamisong@google.com>
2015-04-06 13:35:22 -0700

    Added the /updog service.

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ class ByePage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Goodbye, Cruel World!')
 
+# Every hello leads to a goodbye...
+class UpDogPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write("Nothing much, what's up with you?")
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/bye', ByePage),
+    ('/updog', UpDogPage),
 ], debug=True)
